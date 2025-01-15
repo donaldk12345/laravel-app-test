47b060ab011ce6656c1f57f8d42682f134404bc3
donaldk12345 <fotsoyvesdonald@gmail.com>
2025-01-15 12:47:44 +0100

    go

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required|min:6',
+        ]);
+        if ($validator->fails()) {
+
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 401);
+        } else {
+
+            return User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password'))
+            ]);
+        }
+    }
+
+    public function login(Request $request)
+    {
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'message' => 'Invalid credentials !'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return   response([
+            'message' => 'success',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
+
+
+
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+
+        return response([
+            'message' => 'success'
+        ])->withCookie($cookie);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+        $users = User::with('role')->get();
+
+        $response = [
+            'users' => $users
+        ];
+
+        if ($response != null) {
+            return response()->json($response);
+        } else {
+            return response([
+                'message' => 'Invalid credentials !'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::get('users', [\App\Http\Controllers\AuthController::class, 'index']);
 });
